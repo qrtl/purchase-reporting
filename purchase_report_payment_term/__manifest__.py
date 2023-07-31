145b1e6b80acd4364f64bd99a082410728f86215
Rinaldi Firdaus <rinaldi@quartile.co>
2023-07-31 05:44:18 +0000

    [16.0][ADD] purchase_report_payment_term

--- purchase_report_payment_term/__manifest__.py
+++ purchase_report_payment_term/__manifest__.py
@@ -1,9 +1,9 @@
 # Copyright 2023 Quartile Limited
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    "name": "Purchase Report Date Format",
-    "category": "Reporting",
+    "name": "Purchase Report Payment Term",
     "version": "16.0.1.0.0",
+    "category": "Purchase",
     "author": "Quartile Limited, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-reporting",
     "license": "AGPL-3",
